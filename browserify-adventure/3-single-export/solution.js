11eaa46487a4b884dde0886c45ac0dad5a5575a1
Dylan Smith <dylantsmith@gmail.com>
2015-07-14 22:03:59 -0500

    Fix problem 3
    Refactor the solution to align with the problem statement

--- browserify-adventure/3-single-export/solution.js
+++ browserify-adventure/3-single-export/solution.js
@@ -7,10 +7,7 @@ var input;
 // using the browser's "prompt", pull in a string
 input = prompt("enter a string");
 
-// split the input into an array
-input = input.split(",");
-
-// grab only the unique elements from the array
+// grab only the unique elements from the string
 input = uniquely(input);
 
 console.log(input);
